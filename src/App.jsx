8b39d392b4eb975a450be880a9af255ff38e957c
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2023-11-21 13:29:54 +0530

    calculator version 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Calculator from "./Calculator";
+
+function App() {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Calculator.jsx
@@ -0,0 +1,193 @@
+import { useEffect, useState } from "react";
+
+const Calculator = () => {
+
+  const [display, setDisplay] = useState(" ");
+  const [brdClr, setBrdClr] = useState(false);
+  const allowedCharacters = /^[0-9./*+\- ]*$/;
+
+  const handleDisplay = (e) => {
+    setDisplay(e.target.value);
+  };
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      setDisplay(eval(display));
+    }
+  };
+
+  useEffect(() => {
+    if (!allowedCharacters.test(display)) {
+      setBrdClr(true);
+    } else {
+      setBrdClr(false);
+    }
+  }, [display]);
+  
+  return (
+    <div className="main-container">
+      <h1>Calculator</h1>
+      <div className="container">
+        <div className="user-inp">
+          <input
+            type="text"
+            className={brdClr ? "red-brd" : "black-brd"}
+            onChange={handleDisplay}
+            onKeyDown={handleEnter}
+            value={display}
+          />
+        </div>
+        <div className="user-btn-inp">
+          <div className="cal-row">
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "1");
+              }}
+            >
+              1
+            </button>
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "2");
+              }}
+            >
+              2
+            </button>
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "3");
+              }}
+            >
+              3
+            </button>
+            <button
+              className="btn btn-warning col"
+              onClick={() => {
+                setDisplay(display + " + ");
+              }}
+            >
+              +
+            </button>
+          </div>
+          <div className="cal-row">
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "4");
+              }}
+            >
+              4
+            </button>
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "5");
+              }}
+            >
+              5
+            </button>
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "6");
+              }}
+            >
+              6
+            </button>
+            <button
+              className="btn btn-warning col"
+              onClick={() => {
+                setDisplay(display + " - ");
+              }}
+            >
+              -
+            </button>
+          </div>
+          <div className="cal-row">
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "7");
+              }}
+            >
+              7
+            </button>
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "8");
+              }}
+            >
+              8
+            </button>
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "9");
+              }}
+            >
+              9
+            </button>
+            <button
+              className="btn btn-warning col"
+              onClick={() => {
+                setDisplay(display + " * ");
+              }}
+            >
+              *
+            </button>
+          </div>
+          <div className="cal-row">
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + "0");
+              }}
+            >
+              0
+            </button>
+            <button
+              className="btn btn-primary col"
+              onClick={() => {
+                setDisplay(display + ".");
+              }}
+            >
+              .
+            </button>
+            <button
+              className="btn btn-success col"
+              onClick={() => {
+                setDisplay(eval(display));
+              }}
+            >
+              =
+            </button>
+            <button
+              className="btn btn-warning col"
+              onClick={() => {
+                setDisplay(display + " / ");
+              }}
+            >
+              /
+            </button>
+          </div>
+          <div className="center-btn cal-row">
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                setDisplay("");
+              }}
+            >
+              Clear
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
